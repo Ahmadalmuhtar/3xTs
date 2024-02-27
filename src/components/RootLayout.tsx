76b21cb47e762637b97314b049ed07b32e5631ea
ahmadalmuhtar <ahmadalmuhtar@gmail.com>
2024-02-27 09:50:35 +0100

    removed the whole blog section

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -148,7 +148,6 @@ function Navigation() {
       </NavigationRow>
       <NavigationRow>
         <NavigationItem href="/process">Our Process</NavigationItem>
-        <NavigationItem href="/blog">Blog</NavigationItem>
       </NavigationRow>
     </nav>
   )
